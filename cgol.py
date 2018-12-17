1b588221c68a8f7c2b409d4acd58a94dbec3b322
wemmm <jenny.wem@gmail.com>
2018-12-17 10:04:08 +0000

    Initialise empty board

--- cgol.py
+++ cgol.py
@@ -1,8 +1,8 @@
 ALIVE = "X"
 DEAD = " "
 
-def empty_board():
-  return [ALIVE, DEAD]
+def empty_board(width, height):
+  return [[DEAD for cells in range(height)] for cells in range(width)]
 
 if __name__ == "__main__":
-    print empty_board()
+    print empty_board(10, 10)
