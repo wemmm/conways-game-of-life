debdfaa85c1fe04b98b25432b64fe089eac10fb9
wemmm <jenny.wem@gmail.com>
2019-03-31 17:36:05 +0100

    Next cell state, next board state, run

--- cgol.py
+++ cgol.py
@@ -1,4 +1,6 @@
 import random
+import time
+
 
 ALIVE = "o"
 DEAD = " "
@@ -18,42 +20,73 @@ def random_board(width, height):
     return board
 
 
-def live_neighbours(coordinates, board):
+def get_next_cell_state(coordinates, board):
+    width = len(board)
+    height = len(board[0])
     x = coordinates[0]
     y = coordinates[1]
-    cell = board[x][y]
     live_neighbours = 0
-    if board[x][y + 1] == ALIVE and (y + 1) < len(board):
-        live_neighbours += 1
-    if board[x][y - 1] == ALIVE and (y - 1) >= 0:
-        live_neighbours += 1
-    if board[x + 1][y] == ALIVE and (x + 1) < len(board[0]):
-        live_neighbours += 1
-    if board[x - 1][y] == ALIVE and (x - 1) >= 0:
-        live_neighbours += 1
-    if board[x - 1][y - 1] == ALIVE and (x - 1) >= 0 and (y - 1) >= 0:
-        live_neighbours += 1
-    if board[x + 1][y + 1] == ALIVE and (x + 1) < len(board) and (y + 1) < len(board[0]):
-        live_neighbours += 1
-    if board[x - 1][y + 1] == ALIVE and (x - 1) >= 0 and (y + 1) < len(board[0]):
-        live_neighbours += 1
-    if board[x + 1][y - 1] == ALIVE and (x + 1) < len(board) and (y - 1) >= 0:
-        live_neighbours += 1
-    print board
-    return live_neighbours
+
+    for x1 in range((x-1), (x+1)+1):
+        if x1 < 0 or x1 >= width: continue
+
+        for y1 in range((y-1), (y+1)+1):
+            if y1 < 0 or y1 >= height: continue
+            if x1 == x and y1 == y: continue
+
+            if board[x1][y1] == ALIVE:
+                live_neighbours += 1
+
+    if board[x][y] == ALIVE:
+        if live_neighbours <= 1:
+            return DEAD
+        elif live_neighbours <= 3:
+            return ALIVE
+        else:
+            return DEAD
+    else:
+        if live_neighbours == 3:
+            return ALIVE
+        else:
+            return DEAD
 
 
-def pretty_print(board, width, height):
-    rows = []
-    game_board = board(width, height)
+def next_board_state(board):
+    width = len(board)
+    height = len(board[0])
+    next_board = empty_board(width, height)
+
+    for x in range(0, width):
+        for y in range(0, height):
+            next_board[x][y] = get_next_cell_state((x, y), board)
+
+    return next_board
+
+
+def pretty_print(board):
+    display_as = {
+        DEAD: ' ',
+        ALIVE: 'O'
+    }
+    width = len(board)
+    height = len(board[0])
+    lines = []
     for y in range(0, height):
-        row = ''
+        line = ''
         for x in range(0, width):
-            row += game_board[x][y]
-        rows.append(row)
-    print "\n".join(rows)
+            line += display_as[board[x][y]] * 2
+        lines.append(line)
+    print "\n".join(lines)
+
+
+
+def run(board):
+    next_state = board
+    while True:
+        pretty_print(next_state)
+        next_state = next_board_state(next_state)
+        time.sleep(0.05)
 
 
 if __name__ == "__main__":
-    print live_neighbours([0,0], random_board(3,3))
-    # pretty_print(random_board, 60, 20)
+    run(random_board(60, 20))
